457a9a6e65999bc1ce0303d65a42811a429e36bf
Chris McCarthy <mccart.c.j@gmail.com>
2019-02-05 22:45:41 -0600

    Add open up JMX for use with NR Kafka integration

--- simple-consumer/startConsumer.sh
+++ simple-consumer/startConsumer.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-java -javaagent:target/newrelic.jar -cp "target/newrelic*" -jar target/simple-consumer-1.0-SNAPSHOT-jar-with-dependencies.jar -Dnewrelic.config.file=./newrelic.yml 
-
+java -javaagent:target/newrelic.jar \
+  -cp "target/newrelic*" \
+  -Dcom.sun.management.jmxremote \
+  -Dcom.sun.management.jmxremote.port=9010 \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -jar target/simple-consumer-1.0-SNAPSHOT-jar-with-dependencies.jar 
 
--- simple-producer/startProducer.sh
+++ simple-producer/startProducer.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-java -javaagent:target/newrelic.jar -cp "target/newrelic*" -jar target/simple-producer-1.0-SNAPSHOT-jar-with-dependencies.jar -Dnewrelic.config.file=./newrelic.yml 
-
+java -javaagent:target/newrelic.jar -Dnewrelic.config.file=./newrelic.yml \
+  -Dcom.sun.management.jmxremote \
+  -Dcom.sun.management.jmxremote.port=9010 \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false \
+  -cp "target/newrelic*" \
+  -jar target/simple-producer-1.0-SNAPSHOT-jar-with-dependencies.jar 
 
